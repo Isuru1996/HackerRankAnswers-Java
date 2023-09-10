38dea1cb9134253b0781b5b5d26452c6c888f68b
Isuru1996 <isurug27@gmail.com>
2023-09-10 14:21:21 +0530

    chore: Add answer for The Grid Search question

---
+++ The Grid Search.java
@@ -0,0 +1,212 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'gridSearch' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     *  1. STRING_ARRAY G
+     *  2. STRING_ARRAY P
+     */
+
+    public static String gridSearch(List<String> G, List<String> P) {
+        int mainRows = G.size();
+        int subgridRows = P.size();
+        int mainCols = G.get(0).length();
+        int subgridCols = P.get(0).length();
+
+        for (int i = 0; i <= mainRows - subgridRows; i++) {
+            for (int j = 0; j <= mainCols - subgridCols; j++) {
+                boolean isMatch = true;
+                
+                for (int k = 0; k < subgridRows; k++) {
+                    String mainRow = G.get(i + k).substring(j, j + subgridCols);
+                    String subgridRow = P.get(k);
+                    
+                    if (!mainRow.equals(subgridRow)) {
+                        isMatch = false;
+                        break;
+                    }
+                }
+                
+                if (isMatch) {
+                    // Subgrid found in the main grid
+                    return "YES";
+                }
+            }
+        }
+        
+        // Subgrid not found in the main grid
+        return "NO";
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+                int R = Integer.parseInt(firstMultipleInput[0]);
+
+                int C = Integer.parseInt(firstMultipleInput[1]);
+
+                List<String> G = IntStream.range(0, R).mapToObj(i -> {
+                    try {
+                        return bufferedReader.readLine();
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                })
+                    .collect(toList());
+
+                String[] secondMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+                int r = Integer.parseInt(secondMultipleInput[0]);
+
+                int c = Integer.parseInt(secondMultipleInput[1]);
+
+                List<String> P = IntStream.range(0, r).mapToObj(i -> {
+                    try {
+                        return bufferedReader.readLine();
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                })
+                    .collect(toList());
+
+                String result = Result.gridSearch(G, P);
+
+                bufferedWriter.write(result);
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'gridSearch' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     *  1. STRING_ARRAY G
+     *  2. STRING_ARRAY P
+     */
+
+    public static String gridSearch(List<String> G, List<String> P) {
+        int mainRows = G.size();
+        int subgridRows = P.size();
+        int mainCols = G.get(0).length();
+        int subgridCols = P.get(0).length();
+
+        for (int i = 0; i <= mainRows - subgridRows; i++) {
+            for (int j = 0; j <= mainCols - subgridCols; j++) {
+                boolean isMatch = true;
+                
+                for (int k = 0; k < subgridRows; k++) {
+                    String mainRow = G.get(i + k).substring(j, j + subgridCols);
+                    String subgridRow = P.get(k);
+                    
+                    if (!mainRow.equals(subgridRow)) {
+                        isMatch = false;
+                        break;
+                    }
+                }
+                
+                if (isMatch) {
+                    return "YES";
+                }
+            }
+        }
+        
+        return "NO";
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+                int R = Integer.parseInt(firstMultipleInput[0]);
+
+                int C = Integer.parseInt(firstMultipleInput[1]);
+
+                List<String> G = IntStream.range(0, R).mapToObj(i -> {
+                    try {
+                        return bufferedReader.readLine();
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                })
+                    .collect(toList());
+
+                String[] secondMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+                int r = Integer.parseInt(secondMultipleInput[0]);
+
+                int c = Integer.parseInt(secondMultipleInput[1]);
+
+                List<String> P = IntStream.range(0, r).mapToObj(i -> {
+                    try {
+                        return bufferedReader.readLine();
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                })
+                    .collect(toList());
+
+                String result = Result.gridSearch(G, P);
+
+                bufferedWriter.write(result);
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
