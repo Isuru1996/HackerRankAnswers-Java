17383e603e89c510f2957203b5de70de73b3f7d0
Isuru1996 <isurug27@gmail.com>
2023-09-13 00:55:54 +0530

    chore: Add answer for Jim and the Orders question

---
+++ Jim and the Orders.java
@@ -0,0 +1,79 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'jimOrders' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts 2D_INTEGER_ARRAY orders as parameter.
+     */
+
+    public static List<Integer> jimOrders(List<List<Integer>> orders) {
+        List<Integer> sumList=new ArrayList<Integer>();
+        for(List<Integer> row:orders){
+            sumList.add(row.get(0)+row.get(1));
+        }
+        System.out.println(sumList);
+        List<Integer> sortedList=sumList.stream().sorted().collect(Collectors.toList());
+        
+        List<Integer> outputList=new ArrayList<Integer>();
+        
+        for(Integer value:sumList){
+            for(int i=0;i<sortedList.size();i++){
+                if((value==sortedList.get(i))&&(!(outputList.contains(i+1)))){
+                    outputList.add(i+1);
+                    break;
+                }
+            }
+        }
+        
+        return outputList;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<List<Integer>> orders = new ArrayList<>();
+
+        IntStream.range(0, n).forEach(i -> {
+            try {
+                orders.add(
+                    Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                        .map(Integer::parseInt)
+                        .collect(toList())
+                );
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        List<Integer> result = Result.jimOrders(orders);
+
+        bufferedWriter.write(
+            result.stream()
+                .map(Object::toString)
+                .collect(joining(" "))
+            + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
